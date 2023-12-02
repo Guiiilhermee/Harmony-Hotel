2338abfbf7c4a2ce72924f496807f7b1f5bf2e38
Guiiilhermee rodrigues <guilherme_rodrigues91@hotmail.com>
2023-12-02 06:40:22 +0000

    Added hotel templates and index.html

--- HMS/settings.py
+++ HMS/settings.py
@@ -64,7 +64,12 @@ ROOT_URLCONF = 'HMS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+
+            
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
