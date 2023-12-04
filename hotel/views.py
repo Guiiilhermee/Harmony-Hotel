e7059eaf055da9daf65eae33ea94c6da3eb4ae8b
Guiiilhermee rodrigues <guilherme_rodrigues91@hotmail.com>
2023-12-04 04:43:14 +0000

    Created html templates and deleted old ones

--- hotel/views.py
+++ hotel/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    """ A view to return the index page """
-    return render(request, 'hotel/index.html')
+    """ A view to return the home page """
+    return render(request, 'home.html')
